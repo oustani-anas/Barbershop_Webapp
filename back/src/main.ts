df7abee5e7b876089d3f060941f1e9e7f0786a72
ANAS OUSTANI <anas.migos20@gmail.com>
2024-12-21 17:33:42 +0100

    testing origins

--- back/src/main.ts
+++ back/src/main.ts
@@ -7,9 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: ['https://barbershop-webapp-git-main-oustanis-projects.vercel.app/'],
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Allowed HTTP methods
-    credentials: true, // If you're sending cookies or authorization headers
+    // credentials: true, // If you're sending cookies or authorization headers
   });
 
 
