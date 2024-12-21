b7ecb65255aed965e5c22aa7800ea99f444bf1f6
ANAS OUSTANI <anas.migos20@gmail.com>
2024-12-21 16:25:32 +0100

    adding the correct url for the api after deploying the api 2

--- back/src/main.ts
+++ back/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://barbershop-webapp.onrender.com'],
+    origin: ['http://localhost:3000', 'https://barbershop-webapp-rakfos2f3-oustanis-projects.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Allowed HTTP methods
     credentials: true, // If you're sending cookies or authorization headers
   });
